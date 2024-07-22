9c96306a093422491e698b22244c3af4c9709680
reosfire <2.com9761721@gmail.com>
2024-07-22 16:06:31 +0300

    IntSize times float and contains

--- src/main/kotlin/ru/reosfire/pixorio/extensions/compose/SizeExtensions.kt
+++ src/main/kotlin/ru/reosfire/pixorio/extensions/compose/SizeExtensions.kt
@@ -1,8 +1,17 @@
 package ru.reosfire.pixorio.extensions.compose
 
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
 import kotlin.math.roundToInt
 
 fun Size.toInt() = IntSize(width.toInt(), height.toInt())
 fun Size.round() = IntSize(width.roundToInt(), height.roundToInt())
+
+operator fun IntSize.times(factor: Float) = Size(width * factor, height * factor)
+
+operator fun IntSize.contains(offset: IntOffset) =
+    offset.x in 0..<width && offset.y in 0..<height
+operator fun IntSize.contains(offset: Offset) =
+    offset.x >= 0 && offset.y >= 0 && offset.x < width && offset.y < height

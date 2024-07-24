b5a1b87c975462e2ee7cd55b7229792af607525a
reosfire <2.com9761721@gmail.com>
2024-07-24 20:20:27 +0300

    Empty PaintingTransaction

--- src/main/kotlin/ru/reosfire/pixorio/brushes/PaintingTransaction.kt
+++ src/main/kotlin/ru/reosfire/pixorio/brushes/PaintingTransaction.kt
@@ -6,3 +6,9 @@ interface PaintingTransaction: PreviewTransaction {
     fun apply(editableImage: EditableImage)
     fun revert(editableImage: EditableImage)
 }
+
+object EmptyPaintingTransaction : PaintingTransaction {
+    override fun apply(editableImage: EditableImage) = Unit
+    override fun revert(editableImage: EditableImage) = Unit
+    override fun preview(editableImage: EditableImage) = Unit
+}
